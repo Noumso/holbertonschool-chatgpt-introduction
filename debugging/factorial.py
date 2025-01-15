69ce7445048826f6f75e32ff0ea1d6234e9fc9f4
Noumso <noumane.82300@gmail.com>
2025-01-15 13:50:52 +0000

    fix pycodestyle

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
+
 import sys
 
+
 def factorial(n):
-    """
-    Calculate the factorial of a number.
-
-    Args:
-        n (int): The number to calculate the factorial of.
-
-    Returns:
-        int: The factorial of the number.
-    """
-    result = 1
-
-    while n > 1:
-        result *= n
-        n -= 1
-
-    return result
+    """Calculer le factoriel d'un nombre."""
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./factorial.py <number>")
-        sys.exit(1)
-
-    try:
-        num = int(sys.argv[1])
-        if num < 0:
-            print("Factorial is not defined for negative numbers.")
-            sys.exit(1)
-    except ValueError:
-        print("Please provide a valid integer.")
-        sys.exit(1)
-
-    f = factorial(num)
-    print(f)
-
+f = factorial(int(sys.argv[1]))
+print(f)
